7213bb91600967a98bb79d1d69687d2c04946e76
Koding <hacene.taibi@gmail.com>
2015-11-23 11:42:37 +0000

    scope working 1 variable passed to the view

--- hcnapp/www/js/app.js
+++ hcnapp/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('loginchat', ['ionic'])
+var app = angular.module("starter", ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,9 @@ angular.module('loginchat', ['ionic'])
     }
   });
 })
+
+
+app.controller('superController', ['$scope', function($scope) { 
+  $scope.title = 'Top Sellers in Books'; 
+}]);
+
