ddab3d2a985b6cb3b49f1159776df1eac373c86c
Koding <hacene.taibi@gmail.com>
2015-11-25 11:24:31 +0000

    example of firebase connection

--- hcnapp/www/js/app.js
+++ hcnapp/www/js/app.js
@@ -3,8 +3,12 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module("starter", ['ionic'])
 
+// var app = angular.module("loginchat", ['ionic'], ['firebase'])
+
+var app = angular.module('loginchat', ['ionic', 'firebase']) ;
+
+/*
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -16,10 +20,42 @@ var app = angular.module("starter", ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
 
+});
 
-app.controller('superController', ['$scope', function($scope) { 
-  $scope.title = 'Top Sellers in Books'; 
+*/
+
+app.controller('chatController', ['$scope', 'Message', function($scope, Message){
+    $scope.name = "geekenchef";
+    $scope.messages = Message.all;
+    
+    $scope.inserisci = function(message){
+			Message.create(message);
+		};
 }]);
+
+app.factory('Message', ['$firebase', function($firebase) {
+
+var ref = new Firebase('https://slacking.firebaseio.com/');
+var messages = $firebase(ref.child('messages')).$asArray();
+
+var Message = {
+        all : messages,
+        create : function(message){
+                return messages.$add(message);
+                },
+        get : function(messageId){
+                return $firebase(ref.child('messages').child(messageId)).$asObject();
+                },
+        delete : function(message){
+                return messages.$remove(message);
+                    },
+                };
+
+    return Message;
+}]);
+
+
+
+
 
