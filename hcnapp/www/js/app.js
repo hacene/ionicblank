b62b84f625b350913dc7a29b0d672b7b9e75bee9
Koding <hacene.taibi@gmail.com>
2015-12-06 05:32:13 +0000

    with satellizer

--- hcnapp/www/js/app.js
+++ hcnapp/www/js/app.js
@@ -3,9 +3,11 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('loginchat', ['ionic'])
+var app = angular.module('loginchat', ['ionic', 'satellizer'])
 
-.run(function($ionicPlatform) {
+
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -17,3 +19,54 @@ angular.module('loginchat', ['ionic'])
     }
   });
 })
+
+
+
+
+ app.config(function($authProvider) {
+
+$authProvider.httpInterceptor = function() { return true; },
+$authProvider.withCredentials = true;
+$authProvider.tokenRoot = null;
+$authProvider.cordova = false;
+$authProvider.baseUrl = '/';
+$authProvider.loginUrl = '/auth/login';
+$authProvider.signupUrl = '/auth/signup';
+$authProvider.unlinkUrl = '/auth/unlink/';
+$authProvider.tokenName = 'token';
+$authProvider.tokenPrefix = 'satellizer';
+$authProvider.authHeader = 'Authorization';
+$authProvider.authToken = 'Bearer';
+$authProvider.storageType = 'localStorage';
+
+// Facebook
+$authProvider.facebook({
+  name: 'facebook',
+  url: '/auth/facebook',
+  authorizationEndpoint: 'https://www.facebook.com/v2.5/dialog/oauth',
+  redirectUri: window.location.origin + '/',
+  requiredUrlParams: ['display', 'scope'],
+  scope: ['email'],
+  scopeDelimiter: ',',
+  display: 'popup',
+  type: '2.0',
+  popupOptions: { width: 580, height: 400 }
+});
+
+
+
+    $authProvider.facebook({
+      clientId: '523613521147806'
+    });
+});
+
+
+app.controller('FacebookLogin', function($scope, $auth) {
+
+    $scope.authenticate = function(provider) {
+      $auth.authenticate(provider);
+    };
+
+  });
+  
+  
